870e099891a76e6fe752995932bfee429e576ee9
Lior Avraham <lioravraham7@gmail.com>
2025-05-18 11:01:56 +0300

    Initial Commit

---
+++ WarTests/WarTests.swift
@@ -0,0 +1,17 @@
+//
+//  WarTests.swift
+//  WarTests
+//
+//  Created by Student17 on 17/05/2025.
+//
+
+import Testing
+@testable import War
+
+struct WarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ WarUITests/WarUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WarUITestsLaunchTests.swift
+//  WarUITests
+//
+//  Created by Student17 on 17/05/2025.
+//
+
+import XCTest
+
+final class WarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
